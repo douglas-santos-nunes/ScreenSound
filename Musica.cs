715a26dff5c58f0c52cbcbf560ac990ac3dad049
Douglas <douglasnunesprogramador@gmail.com>
2025-01-05 19:50:09 -0300

    Primeiro commit

---
+++ Musica.cs
@@ -0,0 +1,28 @@
+﻿class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome} ");
+        Console.WriteLine($"Artista: {artista} ");
+        Console.WriteLine($"Duração: {duracao} ");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponível no Plano");
+        }
+        else
+        {
+            Console.WriteLine("Não disponível no Plano");
+        }
+
+    }
+
+    public void ExibirNomeEArtista()
+    {
+        Console.WriteLine($"Nome da Música / Artista: {nome} - {artista}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Musica musica1 = new Musica();
+
+musica1.nome = "666/777";
+musica1.duracao = 220;
+musica1.artista = "Matuê";
+musica1.disponivel = true;
+
+musica1.ExibirNomeEArtista();
